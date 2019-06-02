35ed024797361439d9ea95e8bfac8902246356a3
ShunTakahashi <tshun.1996@icloud.com>
2019-06-02 21:22:28 +0900

    confirm

--- app/helpers/twt_helper.rb
+++ app/helpers/twt_helper.rb
@@ -1,7 +1,7 @@
 module TwtHelper
   def choose_new_or_edit
     if action_name == 'new' ||action_name =='confirm'
-      onfirm_twts_path
+      confirm_twts_path
     elsif action_name == 'edit'
       twt_path
     end
