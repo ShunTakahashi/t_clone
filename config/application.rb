15ebb592ec5979e4781b1a9c168438c4511e94c0
ShunTakahashi <tshun.1996@icloud.com>
2019-06-02 21:43:35 +0900

    time zoon

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :loca
+config.log_level = :info
+
 require_relative 'boot'
 
 require 'rails/all'
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
